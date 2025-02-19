f3bb3b01c0d4695a2a5a7aab2932d7499c8fd152
ashutosh017 <wwwashutosh225@gmail.com>
2025-02-19 12:29:03 +0530

    added equilizer effect for audible tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
-
+// import { LuAudioLines } from "react-icons/lu";
 interface TabInfo {
   id: number;
   url: string;
   title: string | undefined;
-  audible:boolean
+  audible: boolean;
 }
 
 function App() {
@@ -14,21 +15,24 @@ function App() {
   const [search, setSearch] = useState<string>("");
   const [activeTabId, setActiveTabId] = useState<number>();
 
-  
-
   useEffect(() => {
     chrome.tabs.query({}, (tabs) => {
-      const tabsInfo: TabInfo[] = tabs.map((tab) => ({
-        id: tab.id as number,
-        url: tab.url || "",
-        title:tab.title,
-        audible:tab.audible as boolean
-      }));
-      chrome.tabs.query({active:true,currentWindow:true},function(tabs){
+      const tabsInfo: TabInfo[] = tabs.map((tab) => {
+        // const processId = chrome.processes
+        console.log("typeof chrome: ", typeof chrome);
+        const something = {
+          id: tab.id as number,
+          url: tab.url || "",
+          title: tab.title,
+          audible: tab.audible as boolean,
+        };
+        return something;
+      });
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         const activeTab = tabs[0];
         setActiveTabId(activeTab.id);
         console.log(activeTabId);
-      })
+      });
       setTabs(tabsInfo);
       setFilteredTabs(tabsInfo);
       setTabCount(tabsInfo.length);
@@ -42,34 +46,30 @@ function App() {
 
   const handleChange = (val: string) => {
     setSearch(val);
-  
-    const foundTabs = tabs
-      .filter((tab) =>
-        tab.url.toLowerCase().includes(val.toLowerCase()) || 
-        tab.title?.toLowerCase().includes(val.toLowerCase())
-      )
 
-  
+    const foundTabs = tabs.filter(
+      (tab) =>
+        tab.url.toLowerCase().includes(val.toLowerCase()) ||
+        tab.title?.toLowerCase().includes(val.toLowerCase())
+    );
+
     setFilteredTabs(foundTabs);
   };
-  
-
-  
 
   return (
-    <div className="flex flex-col items-center p-6 bg-zinc-900 text-white min-h-screen min-w-80 w-auto h-auto ">
-     <div className="sticky top-0 w-full  text-center">
-     <h1 className="text-2xl font-semibold mb-4">Opened Tabs: {tabCount}</h1>
-      <input
-      autoFocus
-        onChange={(e) => handleChange(e.target.value)}
-        type="text"
-        placeholder="Search for tabs"
-        className="w-full border border-gray-500 bg-zinc-800 text-white p-2 rounded-md mb-4"
-        value={search}
-      />
-     </div>
-      <div className="flex flex-col space-y-2 w-full max-w-md ">
+    <div className="flex flex-col items-center  bg-black text-white min-h-screen min-w-80 w-auto h-auto ">
+      <div className="sticky top-0 w-full bg-black text-center px-4 pt-4 pb-2">
+        <h1 className="text-2xl font-semibold mb-4">Opened Tabs: {tabCount}</h1>
+        <input
+          autoFocus
+          onChange={(e) => handleChange(e.target.value)}
+          type="text"
+          placeholder="Search for tabs"
+          className="w-full border border-gray-500 bg-zinc-800 text-white p-2 rounded-md mb-4"
+          value={search}
+        />
+      </div>
+      <div className="flex flex-col space-y-2 w-full max-w-md px-4 pb-4 pt-0 ">
         {filteredTabs.map((tab) => (
           <button
             key={tab.id}
@@ -77,11 +77,37 @@ function App() {
               e.preventDefault();
               activateTab(tab.id);
             }}
-            className={`flex flex-col w-full text-start ${activeTabId===tab.id? "bg-transparent":"bg-zinc-950"} rounded-lg p-2 text-sm text-gray-100 cursor-pointer  hover:text-blue-500  `}
+            className={`flex w-full text-start  ${
+              activeTabId === tab.id ? "bg-gray-300 text-black font-bold" : "bg-zinc-900 hover:bg-zinc-800"
+              // "bg-transparent"
+            } rounded-lg p-2 text-sm  cursor-pointer justify-between items-center   `}
           >
-           <span className="truncate w-full"> {tab.title}</span>
-            <span className="text-[10px] truncate w-full text-gray-400 italic">{tab.url}</span>
-            {tab.audible && <span>audible</span>}
+            <div className="flex flex-col flex-1   truncate  ">
+              <span className="truncate w-full  ">
+                {" "}
+                {tab.title}
+              </span>
+              <span className="text-[10px] truncate w-full  italic  ">
+                {tab.url}
+              </span>
+            </div>
+            {tab.audible && (
+            <div className="flex space-x-0.5 items-center px-2"> 
+            {[0, 1, 2, 3].map((i) => (
+              <motion.div
+                key={i}
+                className="w-0.5 bg-green-700 rounded"
+                animate={{ height: [2,10,2] }} 
+                transition={{
+                  duration: 0.5,
+                  repeat: Infinity,
+                  ease: "easeInOut",
+                  delay: i * 0.2, 
+                }}
+              />
+            ))}
+          </div>
+            )}
           </button>
         ))}
         {filteredTabs.length === 0 && (
